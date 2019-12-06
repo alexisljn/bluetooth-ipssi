5479d5a5f296434ad7e03f904aca8be3a564e481
Emile Calixte <calixte.emile@gmail.com>
2019-12-06 12:46:19 +0100

    added possibility to delete a device scan

--- mysql_communicator/__init__.py
+++ mysql_communicator/__init__.py
@@ -21,14 +21,14 @@ class connector:
         self.commit()
 
     def get_latest_scan_for_each_mac_address(self):
-        sql = "select mac_address, device_name, rssi, scan_date from scan s where not exists (select 1 from scan s1 where s1.mac_address = s.mac_address and s1.scan_date > s.scan_date) order by scan_date desc"
+        sql = "select mac_address, device_name, rssi, scan_date from scan s where is_deleted = 0 and not exists (select 1 from scan s1 where is_deleted = 0 and s1.mac_address = s.mac_address and s1.scan_date > s.scan_date) order by scan_date desc"
         self.cursor.execute(sql)
         columns = [col[0] for col in self.cursor.description]
         rows = [dict(zip(columns, row)) for row in self.cursor.fetchall()]
         return rows
 
     def get_names_of_mac_address(self, mac_address):
-        sql = "select distinct device_name from scan s where mac_address = %s"
+        sql = "select distinct device_name from scan s where mac_address = %s and is_deleted = 0"
         values = (mac_address, )
         self.cursor.execute(sql, values)
         names = []
@@ -37,9 +37,15 @@ class connector:
         return names
 
     def get_scans_of_mac_address(self, mac_address):
-        sql = "select id, device_name, scan_date, rssi, scanned_by_device from scan s where mac_address = %s order by scan_date desc"
+        sql = "select id, device_name, scan_date, rssi, scanned_by_device from scan s where mac_address = %s and is_deleted = 0 order by scan_date desc"
         values = (mac_address, )
         self.cursor.execute(sql, values)
         columns = [col[0] for col in self.cursor.description]
         rows = [dict(zip(columns, row)) for row in self.cursor.fetchall()]
         return rows
+
+    def delete_scan(self, scan_id):
+        sql = "update scan set is_deleted = 1 where id = %s"
+        values = (scan_id, )
+        self.cursor.execute(sql, values)
+        self.commit()
